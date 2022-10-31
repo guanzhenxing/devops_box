5355616aa6cd2cb0e7be107069768247cc16b627
Jesen Kwan <guan.zhenxing@foxmail.com>
2022-10-31 19:11:00 +0800

    Update docker_install.sh

--- inbox/docker_install.sh
+++ inbox/docker_install.sh
@@ -8,3 +8,7 @@ sudo sh get-docker.sh --mirror Aliyun
 # 启动docker
 sudo systemctl enable docker
 sudo systemctl start docker
+
+# 安装 docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
